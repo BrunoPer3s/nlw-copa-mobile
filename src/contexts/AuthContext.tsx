afc943f52f3a5e64c54ad254bcea78a1696333cf
BrunoPer3s <brunoperes7@hotmail.com>
2022-11-03 14:49:09 -0300

    Google Auth

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode, useContext } from "react"
+import { createContext, ReactNode, useContext, useState, useEffect } from "react"
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
   name: string
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextData {
   user: UserProps
+  isUserLoading: boolean
   signIn: () => Promise<void>
 }
 
@@ -17,17 +23,44 @@ interface AuthContextProviderProps {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '233745138380-ohc681vsuubcq3vn7tifia4j6ln3bi2l.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
+
   async function signIn() {
-    console.log('entrou');
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+
+    } catch (err) {
+      console.log(err);
+      throw err
+
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log(`Token de autenticação: ${access_token}`);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: 'Bruno',
-        avatarUrl: 'https://github.com/brunoper3s.png'
-      }
+      isUserLoading,
+      user
     }}>
       {children}
     </AuthContext.Provider>
